82a45db06d8726fa4ecb55c46d31754a9a5fcd9d
LGocool <67392650+LGocool@users.noreply.github.com>
2023-02-07 13:21:13 -0500

    passed all 9 tests in the functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+ function saturdayFun (activity = "roller-skate"){
+    let message = activity ;
+    return (`This Saturday, I want to ${message}!`);
+ }
+ function mondayWork (Mwork = "go to the office"){
+    return (`This Monday, I will ${Mwork}.`);
+ }
+ 
+ function wrapAdjective(part1 = "*"){
+   const innerFunction = function (part2 = "special"){
+      return(`You are ${part1}${part2}${part1}!`);
+    };
+    return innerFunction;
+    }
+
+    
